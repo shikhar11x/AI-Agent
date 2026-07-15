a80c57d6a75209de720bb4a26e7d4d674836823c
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-07-15 20:26:13 +0530

    Uploading

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ BASE_URL = "https://api.groq.com/openai/v1"
 
 MODEL_NAME = "llama-3.3-70b-versatile"
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
